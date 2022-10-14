144cabdbae26d444ed8b8f06d592e2f09d20f219
Dmitriy <zeybert90@mail.ru>
2022-10-14 19:54:57 +0300

    25

---
+++ Задача 25/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Step ( int num, int step)
+{
+    int a = num;
+    for(int i = 1; i != step; i ++)
+    {
+        a *= num;       
+    }
+    return a;
+    
+}
+
+Console.Clear();
+Console.WriteLine("Введите возводимое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень числа: ");
+int B = int.Parse(Console.ReadLine()!);
+int result = Step(A, B);
+Console.Write(result);
