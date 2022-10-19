7509883e90a86fe04da8830b6baa68bcd5f33f15
Dmitriy <zeybert90@mail.ru>
2022-10-20 00:14:32 +0300

    Задача со звездочкой

---
+++ Задача со звездочкой/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача “со звездочкой”: Напишите функцию, которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+void Mass( int num)
+{
+    string sap = "*";
+    string space = " ";
+    string sup = String.Empty;
+    int n = num;
+    for(int j = 0; j < num; j++)
+    {
+    
+        for(int i = 0; i < n; i++)
+        {
+            Console.Write(space);
+        }
+    Console.Write(sup);
+    sup = sup + sap;
+    Console.Write(sup);
+    n--;
+    Console.WriteLine();
+    }
+
+}
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Mass(n);
+
+
+
